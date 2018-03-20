21a21f1c0367158fec19879b97da62539c0c2a77
Du <dhaishan@8c85903983d5.ant.amazon.com>
2018-03-21 09:37:49 +1100

    Change to new local file

--- docker_cmd.sh
+++ docker_cmd.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/sh
-docker run -it -v /root/tushare:/tushare python3-mxnet-tushare $1
+docker run -it -v /Users/dhaishan/Project/ubuntu-python3-tushare-mxnet:/tushare python3-mxnet-tushare $1
